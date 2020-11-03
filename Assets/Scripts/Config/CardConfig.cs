c2e93daa55205339fb1ef4f5e2c1c8242d171cbb
shuiyuecanhua <34645087+shuiyuecanhua@users.noreply.github.com>
2020-11-03 20:25:15 +0800

    配置表

--- Assets/Scripts/Config/CardConfig.cs
+++ Assets/Scripts/Config/CardConfig.cs
@@ -1,21 +1,44 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
-
-//[SerializeField]
-public class CardConfigList
+  
+public static class CardConfigManager
 {
-    public string configname= "CardConfig";
-    public List<CardConfig> List;
-    public CardConfigList()
+    public static Dictionary<int,MonsterCardConfig.Config> MonsterConfig;
+    public static void InintConfig()
     {
-        List = new List<CardConfig>();
-        configname = "CardConfig";
+        string configpath = Application.dataPath + "/Config/MonsterCardConfig.json";
+        List<MonsterCardConfig.Config> list;
+        if (File.Exists(configpath))
+        { 
+            list = JsonUtility.FromJson<MonsterCardConfig>(File.ReadAllText(configpath)).List;
+            foreach (var v in list)
+            {
+                MonsterConfig.Add(v.id,v);
+            }
+        }
+        else
+        {
+            MonsterConfig = new Dictionary<int, MonsterCardConfig.Config>();
+        }
+    } 
+    
+}
+[Serializable]
+public class MonsterCardConfig
+{
+    [SerializeField]
+    public  List<Config> List = new List<Config>();
+    [Serializable]
+    public class Config
+    {
+        public int id = 1;
+        public string name;
+        public string desc;
+        public int atk = 90;
+        public int def = 90;
     }
 }
-public class CardConfig
-{
-    public string name="1";
-    public int atk=90;
-    public int def=80;
-}
+
