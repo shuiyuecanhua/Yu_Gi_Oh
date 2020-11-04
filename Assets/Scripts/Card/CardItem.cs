ae11110386eed061fdae0f5acaf05adc99016106
shuiyuecanhua <34645087+shuiyuecanhua@users.noreply.github.com>
2020-11-05 00:01:23 +0800

    Revert "Revert "配置表""
    This reverts commit a0668f900b165064b4f6eb7c9acc02654a9450c5.

---
+++ Assets/Scripts/Card/CardItem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card
+{
+    
+}
+public class MonsterCard: Card
+{
+    public MonsterCardConfig.Config cfg;
+    public MonsterCard(int id)
+    {
+        cfg= CardConfigManager.MonsterConfig[id];
+    }
+}
+public class EffectCard: Card
+{
+
+}
--- Assets/Scripts/ConfigEditorWindow.cs
+++ Assets/Scripts/ConfigEditorWindow.cs
@@ -17,59 +17,125 @@ public class ConfigEditorWindow : EditorWindow
     private string configname;
     private string configpath;
     private string[] keys;
+    public bool IsInit;
     FieldInfo[] fArray;
-    // Start is called before the first frame update
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    Type tchild;
+    Type type;
+    Type listtype;
+    System.Object config;
+    IEnumerable configlist;
+    Vector2 scrollpos;
+     
     private void OnGUI()
     {
         EditorGUILayout.BeginVertical();
         EditorGUILayout.BeginHorizontal();
         EditorGUILayout.LabelField("配置名字:");
         configname=EditorGUILayout.TextField(configname);
-        configpath = Application.dataPath + "configpath/"+ configname+".json";
-        
-        
+        configpath = Application.dataPath + "/Config/" + configname+".json"; 
         if (GUILayout.Button("确定"))
         {
-            Type t = Type.GetType(configname);
-            if (t == null)
+            type= Type.GetType(configname);
+            if (type == null)
             {
                 ShowNotification(new GUIContent("当前没有此类:"+ configname));
                 return;
-            }
-            var v = new object();
-            v = Activator.CreateInstance(t);
+            } 
+            var _config = Activator.CreateInstance(type);
             if (File.Exists(configpath))
             {
-                v=JsonUtility.FromJson(File.ReadAllText(configpath), t);
+                _config = JsonUtility.FromJson(File.ReadAllText(configpath), type);
             }
-            FieldInfo targetPP = t.GetField("configname");
-            string tclassname= (string)targetPP.GetValue(v);
-            Type tchild = Type.GetType(tclassname);
+            tchild = Type.GetType(configname + "+Config"); 
+            FieldInfo configpp = type.GetField("List");
+            System.Object v = configpp.GetValue(_config);
+            listtype = configpp.FieldType; 
+            configlist = v as IEnumerable;
+            config = _config;
             fArray = tchild.GetFields();
             return;
         }
+        if (config == null)
+            return;
+        if (config!=null)
+        {
+            if (GUILayout.Button("保存"))
+            {
+                if (config == null) return;
+                string jsonstr = JsonUtility.ToJson(config);
+                if (File.Exists(configpath))
+                {
+                    File.Delete(configpath);
+                }
+                File.WriteAllText(configpath, jsonstr);
+                AssetDatabase.SaveAssets();
+            }
+            if (GUILayout.Button("添加"))
+            {
+                Type t = tchild;
+                object item;
+                MethodInfo method = listtype.GetMethod("Add");
+                item = Activator.CreateInstance(t);
+                object[] tempt = new object[1];
+                tempt[0] = item;
+                method.Invoke(configlist, tempt);
+            }
+        }
         EditorGUILayout.EndHorizontal();
         EditorGUILayout.EndVertical();
-        if (fArray == null)
-        {
-            return;
-        }
 
+        scrollpos= EditorGUILayout.BeginScrollView(scrollpos);
 
         EditorGUILayout.BeginVertical();
         EditorGUILayout.BeginHorizontal();
-        
-        foreach (var v in fArray)
+
+        foreach (var _v in fArray)
         {
-            EditorGUILayout.LabelField(v.Name,GUILayout.Width(100));
-        } 
+            EditorGUILayout.LabelField(_v.Name, GUILayout.Width(100));
+        }
+
         EditorGUILayout.EndHorizontal();
         EditorGUILayout.EndVertical();
-        
+
+        EditorGUILayout.BeginVertical();
+        int index = 0;
+        foreach (var _v in configlist)
+        {
+            EditorGUILayout.BeginHorizontal();
+            foreach (var _v2 in fArray)
+            {
+                FieldInfo targetPP = tchild.GetField(_v2.Name);
+                object value;  
+                if(targetPP.FieldType.Name== "Int32")
+                {
+                    value = int.Parse(targetPP.GetValue(_v).ToString());
+                    value = EditorGUILayout.IntField((int)value, GUILayout.Width(100));
+                }
+                else
+                {
+                    object obj = targetPP.GetValue(_v);
+                    if (obj == null)
+                    {
+                        obj = "";
+                    }
+                    value = obj.ToString();
+                    value = EditorGUILayout.TextField((string)value, GUILayout.Width(100));
+                }
+                targetPP.SetValue(_v, value);
+            }
+            if (GUILayout.Button("删除",GUILayout.Width(100)))
+            {
+                MethodInfo method = listtype.GetMethod("RemoveAt"); 
+                object[] tempt = new object[1];
+                tempt[0] = index;
+                method.Invoke(configlist, tempt);
+                return;
+            }
+            index++;
+            EditorGUILayout.EndHorizontal();
+        }
+        EditorGUILayout.EndVertical();
+        EditorGUILayout.EndScrollView();
+
     }
 }
